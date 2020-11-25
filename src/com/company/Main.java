8443cf7a3b585cc44ec9a9531327ae391ee18abc
J W <Joshwa97@gmail.com>
2020-11-25 00:20:33 -0600

    Created basic functionality for comparsion between floats

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,53 @@ public class Main {
             String[] parts = in.split("'", 3);
             System.out.println(parts[1]);
         }
+        in = "2>3";
 
+        if(in.matches("\\d*\\.*\\d*<*>*=*\\d*\\.*\\d*"))
+        {
+            System.out.println(interpretLine(in,true));
+        }
+
+    }
+    public static boolean interpretLine(String line, boolean f)
+    {
+        if(line.matches("\\d*\\.*\\d*<\\d*\\.*\\d*"))
+        {
+            double x = Double.parseDouble(line.split("<")[0]);
+            double y = Double.parseDouble(line.split("<")[1]);
+            return x<y;
+        }
+        else if(line.matches("\\d*\\.*\\d*<=\\d*\\.*\\d*"))
+        {
+            double x = Double.parseDouble(line.split("<=")[0]);
+            double y = Double.parseDouble(line.split("<=")[1]);
+            return x<=y;
+        }
+        else if(line.matches("\\d*\\.*\\d*>\\d*\\.*\\d*"))
+        {
+            double x = Double.parseDouble(line.split(">")[0]);
+            double y = Double.parseDouble(line.split(">")[1]);
+            return x>y;
+        }
+        else if(line.matches("\\d*\\.*\\d*>=\\d*\\.*\\d*"))
+        {
+            double x = Double.parseDouble(line.split(">=")[0]);
+            double y = Double.parseDouble(line.split(">=")[1]);
+            return x>=y;
+        }
+        else if(line.matches("\\d*\\.*\\d*==\\d*\\.*\\d*"))
+        {
+            double x = Double.parseDouble(line.split("==")[0]);
+            double y = Double.parseDouble(line.split("==")[1]);
+            return x<=y;
+        }
+        else if(line.matches("\\d*\\.*\\d*!=\\d*\\.*\\d*"))
+        {
+            double x = Double.parseDouble(line.split("!=")[0]);
+            double y = Double.parseDouble(line.split("!=")[1]);
+            return x!=y;
+        }
+
+        return false;
     }
 }
