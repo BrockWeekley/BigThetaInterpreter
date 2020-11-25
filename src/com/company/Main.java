6e27429e6138bef22c57552a470eeebd7a86bb86
unknown <brockweekley@gmail.com>
2020-11-24 21:20:05 -0600

    print statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        String in = "print('Hello World')";
+        if(in.contains("print")) {
+            String[] parts = in.split("'", 3);
+            System.out.println(parts[1]);
+        }
 
     }
 }
