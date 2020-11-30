92c6d384e6f84ad07396165c6cc007e7b497718c
Robert Lincoln Truesdale III <31676518+tretrue@users.noreply.github.com>
2020-11-30 12:57:22 -0600

    first rendition of if/else parser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,9 @@
 package com.company;
+import java.lang.Object;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 
 public class Main {
 
@@ -8,6 +13,58 @@ public class Main {
         if(in.contains("print")) {
             String[] parts = in.split("'", 3);
             System.out.println(parts[1]);
+        }
+
+        try {
+            File myObj = new File("src/test.py");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+
+                if(data.contains("if")){
+                    System.out.println("found an if");
+                    String condition  = data.split("[\\(\\)]")[1];
+                    System.out.println("evaluating "+condition+" it's false.");
+                    //evaluate(condition);
+                    //fake condition for testing if above condition is true/false
+                    Boolean con = false;
+                    if(con){
+                        while(true) {
+                            data = myReader.nextLine();
+                            //end if with blank line or an else:
+                            if(data.equals("")) {
+                                break;
+                            }
+                            else if(!data.contains("else")){
+                                //execute(data);
+                                System.out.println("\nnow executing " + data);
+                            }
+                            else{
+                                break;
+                            }
+                        }
+                    }
+                    else{
+                        while(true) {
+                            data = myReader.nextLine();
+                            if(data.contains("else")){
+                                data = myReader.nextLine();
+                               // execute(data);
+                                System.out.println("\nnow executing " + data);
+                            }
+                            break;
+                        }
+
+                    }
+
+
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
 
     }
---
+++ src/test.py
@@ -0,0 +1,4 @@
+if (3 == 5):
+    print("YOOO 3 = 5!")
+else:
+    print("hey man,  3 != 5...")
