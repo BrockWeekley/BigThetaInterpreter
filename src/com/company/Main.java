35ca82921aaf5126b0cca08980dbac7c8274921b
Brad Schinker <bms67r@mail.missouri.edu>
2020-11-29 19:49:44 -0600

    arithmetic interpretation function built

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.Arrays;
+import java.lang.Math;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,11 +12,19 @@ public class Main {
             String[] parts = in.split("'", 3);
             System.out.println(parts[1]);
         }
-        in = "2>3";
+        in = "2 > 3";
 
-        if(in.matches("\\d*\\.*\\d*<*>*=*\\d*\\.*\\d*"))
+        if(in.matches("\\d*\\.*\\d*\\s*<*>*=*\\s*\\d*\\.*\\d*"))
         {
             System.out.println(interpretLine(in,true));
+        }
+
+        in = "(2*2) + (3*3)";
+
+        if (in.contains("+") || in.contains("-") || in.contains("*") || in.contains("/") || in.contains("%") || in.contains("^")){
+            //String[] mathStrings = (in.replaceAll("\\s","")).split("((?<=[-+*/%^])|(?=[-+*/%^]))(?![^\\(\\[]*[\\]\\)])");
+            //System.out.print(Arrays.toString(mathStrings));
+            System.out.println(interpretMath(in.replaceAll("\\s","")));
         }
 
     }
@@ -57,5 +68,80 @@ public class Main {
         }
 
         return false;
+    }
+
+    public static double interpretMath(String in){
+        String[] mathStrings = in.split("((?<=[-+*/%^])|(?=[-+*/%^]))(?![^\\(\\[]*[\\]\\)])");
+        System.out.print(Arrays.toString(mathStrings));
+
+        double result = 0;
+        String operation = "";
+        for(String eq : mathStrings){
+            if(eq.matches("\\(([^()]+)\\)")){
+                double parResult = interpretMath(eq.replaceAll("[()]", ""));
+                switch (operation){
+                    case "+":
+                        result += parResult;
+                        break;
+                    case "-":
+                        result -= parResult;
+                        break;
+                    case "*":
+                        result *= parResult;
+                        break;
+                    case "/":
+                        result /= parResult;
+                        break;
+                    case "%":
+                        result %= parResult;
+                        break;
+                    case "^":
+                        result = Math.pow(result, parResult);
+                        break;
+                    default:
+                        result = parResult;
+                }
+            }
+            else if(eq.matches("\\d*\\.*\\d")){
+                double toNum = Double.parseDouble(eq);
+                if(!(operation.equals("")))
+                    result = performOperaton(operation, result, toNum);
+                else
+                    result = toNum;
+            }
+            else{
+                operation = eq;
+            }
+        }
+        return result;
+    }
+
+    public static double performOperaton(String operation, double firstNum, double secondNum){
+        double result = 0;
+
+        switch (operation){
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case "-":
+                result = firstNum - secondNum;
+                break;
+            case "*":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                result = firstNum / secondNum;
+                break;
+            case "%":
+                result = firstNum % secondNum;
+                break;
+            case "^":
+                result = Math.pow(firstNum, secondNum);
+                break;
+            default:
+                System.out.println("Something went wrong with " + operation);
+                result = 0;
+        }
+        return result;
     }
 }
